78033cd17c3d107e7a1453fc14b20dc76ca0dd4f
Kumar Aditya <59607654+kumaraditya303@users.noreply.github.com>
2021-08-19 17:55:39 +0530

    Add tests

--- pytest_playwright_snapshot/plugin.py
+++ pytest_playwright_snapshot/plugin.py
@@ -1,42 +1,39 @@
 import sys
 from io import BytesIO
 from pathlib import Path
+from typing import Any, Callable
 
 import pytest
-from _pytest.config import Config
-from _pytest.config.argparsing import Parser
-from _pytest.fixtures import SubRequest
-from _pytest.nodes import get_fslocation_from_item
 from PIL import Image
 from pixelmatch.contrib.PIL import pixelmatch
 
 
 @pytest.fixture
-def assert_snapshot(pytestconfig: Config, request: SubRequest, browser_name: str):
-    def compare(img: bytes, name: str):
+def assert_snapshot(pytestconfig: Any, request: Any, browser_name: str) -> Callable:
+    def compare(img: bytes, name: str, *, threshold: float = 0.1) -> None:
         update_snapshot = pytestconfig.getoption("--update-snapshots")
         filepath = (
-            Path(request.node.fspath).parent
+            Path(request.node.fspath).parent.resolve()
             / "__snapshots__"
             / browser_name
             / sys.platform
         )
         filepath.mkdir(parents=True, exist_ok=True)
+        file = filepath / name
         if update_snapshot:
-            with open(filepath / name, mode="wb") as f:
-                f.write(img)
+            file.write_bytes(img)
             return
-        if not (filepath / name).exists():
-            pytest.fail("Snapshot not found, use --update-snapshots to create it.")
+        if not file.exists():
+            pytest.fail("Snapshot not found, use --update-snapshots to update it.")
         image = Image.open(BytesIO(img))
-        golden = Image.open(filepath / name)
-        diff_pixels = pixelmatch(image, golden, includeAA=True)
-        assert diff_pixels == 0, "Snapshot does not match"
+        golden = Image.open(file)
+        diff_pixels = pixelmatch(image, golden, threshold=threshold)
+        assert diff_pixels == 0, "Snapshots does not match"
 
     return compare
 
 
-def pytest_addoption(parser: Parser) -> None:
+def pytest_addoption(parser: Any) -> None:
     group = parser.getgroup("playwright-snapshot", "Playwright Snapshot")
     group.addoption(
         "--update-snapshots",
