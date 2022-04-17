b82b23365538d3f22c4d37dc1a43fb68427b4841
symonstorozhenko <symon.storozhenko@gmai.com>
2022-04-17 17:21:50 -0400

    linted and added comments

--- pytest_playwright_snapshot/plugin.py
+++ pytest_playwright_snapshot/plugin.py
@@ -4,6 +4,7 @@ import shutil
 from io import BytesIO
 from pathlib import Path
 from typing import Any, Callable
+
 import pytest
 from PIL import Image
 from pixelmatch.contrib.PIL import pixelmatch
@@ -17,21 +18,21 @@ def assert_snapshot(pytestconfig: Any, request: Any, browser_name: str) -> Calla
         update_snapshot = pytestconfig.getoption("--update-snapshots")
         test_file_name = str(os.path.basename(request.node.fspath)).strip('.py')
         filepath = (
-            Path(request.node.fspath).parent.resolve()
-            / 'snapshots'
-            / test_file_name
+                Path(request.node.fspath).parent.resolve()
+                / 'snapshots'
+                / test_file_name
         )
         filepath.mkdir(parents=True, exist_ok=True)
         file = filepath / name
-
+        # Create a dir where all snapshot test failures will go
         results_dir_name = (Path(request.node.fspath).parent.resolve()
-                / "snapshots_tests_failures"
-        )
+                            / "snapshots_tests_failures"
+                            )
         test_results_dir = (
                 results_dir_name
-                /test_file_name/test_name
+                / test_file_name / test_name
         )
-
+        # Remove a single test's past run dir with actual, diff and expected images
         if test_results_dir.exists():
             shutil.rmtree(test_results_dir)
         if update_snapshot:
@@ -43,18 +44,16 @@ def assert_snapshot(pytestconfig: Any, request: Any, browser_name: str) -> Calla
         img_a = Image.open(BytesIO(img))
         img_b = Image.open(file)
         img_diff = Image.new("RGBA", img_a.size)
-
         mismatch = pixelmatch(img_a, img_b, img_diff, includeAA=True, threshold=threshold)
-
-        if mismatch == 0:
-            "Snapshots match!"
-        else:
-
+        try:
+            assert mismatch == 0
+        except:
+            # Create new test_results folder
             test_results_dir.mkdir(parents=True, exist_ok=True)
             img_diff.save(f'{test_results_dir}/Diff_{name}')
             img_a.save(f'{test_results_dir}/Actual_{name}')
             img_b.save(f'{test_results_dir}/Expected_{name}')
-            pytest.fail("Snapshots DO NOT match!")
+            assert mismatch == 0, "Snapshots DO NOT match!"
 
     return compare
 
