08b617973a1c5ce24f53d640cf1a04bca58b93be
symonstorozhenko <symon.storozhenko@gmai.com>
2022-04-24 17:20:13 -0400

    added ci tests and fail on  --update-snapshots

--- pytest_playwright_snapshot/plugin.py
+++ pytest_playwright_snapshot/plugin.py
@@ -4,7 +4,6 @@ import shutil
 from io import BytesIO
 from pathlib import Path
 from typing import Any, Callable
-
 import pytest
 from PIL import Image
 from pixelmatch.contrib.PIL import pixelmatch
@@ -12,48 +11,48 @@ from pixelmatch.contrib.PIL import pixelmatch
 
 @pytest.fixture
 def assert_snapshot(pytestconfig: Any, request: Any, browser_name: str) -> Callable:
-    test_name = f"{str(request.node.name)}[{str(sys.platform)}]"
+    test_name = f"{str(Path(request.node.name))}[{str(sys.platform)}]"
+    test_dir = str(Path(request.node.name)).split('[', 1)[0]
 
-    def compare(img: bytes, *, threshold: float = 0.1, name=f'{test_name}.png') -> None:
+    def compare(img: bytes, *, threshold: float = 0., name=f'{test_name}.png') -> None:
         update_snapshot = pytestconfig.getoption("--update-snapshots")
-        test_file_name = str(os.path.basename(request.node.fspath)).strip('.py')
+        test_file_name = str(os.path.basename(Path(request.node.fspath))).strip('.py')
         filepath = (
                 Path(request.node.fspath).parent.resolve()
                 / 'snapshots'
                 / test_file_name
+                / test_dir
         )
         filepath.mkdir(parents=True, exist_ok=True)
         file = filepath / name
         # Create a dir where all snapshot test failures will go
         results_dir_name = (Path(request.node.fspath).parent.resolve()
-                            / "snapshots_tests_failures"
-                            )
-        test_results_dir = (
-                results_dir_name
-                / test_file_name / test_name
-        )
+                            / "snapshot_tests_failures")
+        test_results_dir = (results_dir_name
+                            / test_file_name / test_name)
         # Remove a single test's past run dir with actual, diff and expected images
         if test_results_dir.exists():
             shutil.rmtree(test_results_dir)
         if update_snapshot:
             file.write_bytes(img)
-            return
+            pytest.fail("--> Snapshots updated. Please review images")
         if not file.exists():
             file.write_bytes(img)
-            pytest.fail("Snapshot not found, created new one. To update, use --update-snapshots")
+            # pytest.fail(
+            pytest.fail("--> New snapshot(s) created. Please review images")
         img_a = Image.open(BytesIO(img))
         img_b = Image.open(file)
         img_diff = Image.new("RGBA", img_a.size)
-        mismatch = pixelmatch(img_a, img_b, img_diff, includeAA=True, threshold=threshold)
-        try:
-            assert mismatch == 0
-        except:
+        mismatch = pixelmatch(img_a, img_b, img_diff, threshold=threshold)
+        if mismatch == 0:
+            return
+        else:
             # Create new test_results folder
             test_results_dir.mkdir(parents=True, exist_ok=True)
             img_diff.save(f'{test_results_dir}/Diff_{name}')
             img_a.save(f'{test_results_dir}/Actual_{name}')
             img_b.save(f'{test_results_dir}/Expected_{name}')
-            assert mismatch == 0, "Snapshots DO NOT match!"
+            pytest.fail("--> Snapshots DO NOT match!")
 
     return compare
 
